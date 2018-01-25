5e3a6a38a6140a2afd1432b1a9651906e34ef029
DavidDeprost <dadeprost@gmail.com>
2018-01-25 20:54:18 +0100

    Implement regex for finding lines with time encoding

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
+extern crate regex;
+use regex::Regex;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::BufReader;
 
-fn help() {
-    println!("
-Submod
-
-Usage: submod inputfile seconds
-  inputfile: .srt or .vtt subtitle file
-  seconds: seconds to add or subtract from time encoding
-");
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -40,12 +34,29 @@ fn main() {
 
 }
 
-fn convert_srt(input: &str, seconds: f64) {
-    let mut f = File::open(input).expect("File not found.");
+fn convert_srt(input: &str, _seconds: f64) {
+    let f = File::open(input).expect("File not found.");
+    let reader = BufReader::new(f);
 
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("Something went wrong reading the file.");
+    let re = Regex::new(r"\d{2}:\d{2}:\d{2},\d{3}").unwrap();
 
-    println!("File contents:\n{}", contents);
+    for line in reader.lines() {
+        let old_line = line.expect("Error reading line.");
+        let timeline: bool = re.is_match(&old_line);
+        
+        if timeline {
+            println!("{}", old_line);
+        }
+    }
+
+}
+
+fn help() {
+    println!("
+Submod
+
+Usage: submod inputfile seconds
+  inputfile: .srt or .vtt subtitle file
+  seconds: seconds to add or subtract from time encoding
+");
 }
