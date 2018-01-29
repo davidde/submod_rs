02bf63e77d3fffecce1281c9c08427731b82b890
DavidDeprost <dadeprost@gmail.com>
2018-01-29 19:09:02 +0100

    Implement function for processing time fields

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 extern crate regex;
 use regex::Regex;
 
-extern crate clap; 
-use clap::{App, Arg,};
+extern crate clap;
+use clap::{App, Arg, AppSettings};
 
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-
+use std::time::Duration;
 
 fn main() {
     let matches = App::new("submod")
+        // AllowLeadingHyphen allows passing negative seconds:
+        .setting(AppSettings::AllowLeadingHyphen)
         .version("0.1.0")
-        .about("Modifies time encoding of movie subtitles")
-        .author("David Deprost")
+        .about("Modify the time encoding of movie subtitles.")
         .arg(Arg::with_name("INPUTFILE")
             .help("The .srt or .vtt inputfile to convert")
             .required(true)
@@ -53,21 +54,21 @@ fn main() {
 
     // These bools can be ignored if they are the same type as the inputfile;
     // they signify to convert to the opposite extension than the input:
-    // e.g. if vtt == true, we convert the srt input to vtt.
-    let vtt: bool = matches.is_present("vtt");
-    let srt: bool = matches.is_present("srt");
+    // e.g. if to_vtt == true, we convert the srt input to vtt.
+    let to_vtt: bool = matches.is_present("vtt");
+    let to_srt: bool = matches.is_present("srt");
 
     if inputfile.ends_with(".srt") {
         convert_srt(inputfile, seconds)
     }
 
     println!("Change file {} with {} seconds", inputfile, seconds);
-    if vtt {
+    if to_vtt {
         println!("Converting the file to .vtt!");
     }
 }
 
-fn convert_srt(input: &str, _seconds: f64) {
+fn convert_srt(input: &str, seconds: f64) {
     let f = File::open(input).expect("File not found.");
     let reader = BufReader::new(f);
 
@@ -79,9 +80,13 @@ fn convert_srt(input: &str, _seconds: f64) {
         let timeline: bool = re.is_match(&old_line);
 
         if timeline {
-            // println!("{}", old_line);
+            let new_line = old_line.replace(",", ".");
+            //let new_line = process_line(&new_line, seconds);
+            
         }
     }
+    let test = process_time("00:00:12.512", seconds);
+    println!("Processed test: {}", test);
 
 }
 
@@ -92,4 +97,76 @@ USAGE:
         INPUTFILE: .srt or .vtt subtitle file
         SECONDS: seconds to add or subtract from time encoding
 ");
+}
+
+// fn process_line(line: &str, seconds: f64) -> String {
+
+// }
+
+fn process_time(time: &str, incr: f64) -> String {
+    let hours: u64 = match time[0..2].parse() {
+        Ok(n) => {
+            n
+        },
+        Err(_) => {
+            panic!("error: invalid hour field in timeline");
+        },
+    };
+
+    let mins: u64 = match time[3..5].parse() {
+        Ok(n) => {
+            n
+        },
+        Err(_) => {
+            panic!("error: invalid minutes field in timeline");
+        },
+    };
+
+    // We convert the secs to millisecs, because the Duration functions
+    // require an integer u64 argument.
+    let millis: u64 = match time[6..12].parse::<f64>() {
+        Ok(n) => {
+            let n = 1000.0 * n;
+            let n = n as u64;
+            n
+        },
+        Err(_) => {
+            panic!("error: invalid seconds field in timeline");
+        },
+    };
+
+    let hours = Duration::from_secs(hours*3600);
+    let mins = Duration::from_secs(mins*60);
+    let secs = Duration::from_millis(millis);
+
+    let new_time = if incr >= 0.0 {
+        let incr = (incr*1000.0) as u64;
+        let incr = Duration::from_millis(incr);
+        hours + mins + secs + incr
+    } else {
+        let incr = (incr*(-1000.0)) as u64;
+        let incr = Duration::from_millis(incr);
+        // will panic when negative!
+        if hours + mins + secs < incr {
+            Duration::from_millis(0)
+        } else {
+            hours + mins + secs - incr
+        }
+    };
+
+    let new_time = new_time.as_secs() as f64
+           + new_time.subsec_nanos() as f64 * 1e-9;
+
+    let time_string = if new_time > 0.0 {
+        let hours = new_time as u64 / 3600;
+        let mins = (new_time as u64 % 3600) / 60;
+        let secs = new_time % 60.0;
+        format!("{0:02}:{1:02}:{2:06.3}", hours, mins, secs)
+    } else {
+        // the subtitles are now scheduled before the start
+        // of the movie, so we can delete them:
+        String::from("(DELETED)\n")
+    };
+
+    return time_string;
 }
